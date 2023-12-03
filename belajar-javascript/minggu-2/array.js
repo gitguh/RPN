9bf399c290381d0c523277bd02bb7df67a93f903
hug <guh.sanjaya@proton.me>
2023-12-03 12:54:27 +0700

    commit

--- belajar-javascript/minggu-2/array.js
+++ belajar-javascript/minggu-2/array.js
@@ -2,6 +2,9 @@
 
 Referensi:
 
+Programmer Zaman Now - TUTORIAL JAVASCRIPT DASAR BAHASA INDONESIA
+https://www.youtube.com/watch?v=SDROba_M42g&t=8010s
+
 Programmer Zaman Now - TUTORIAL JAVASCRIPT STANDARD LIBRARY (BAHASA INDONESIA)
 https://www.youtube.com/watch?v=oFTeb4Lkwek&t=1256s
 
@@ -10,5 +13,8 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 
 W3Schools:
 https://www.w3schools.com/js/js_array_methods.asp
+
+Discord RPN:
+https://discord.com/channels/1129078950589628476/1129091799311458364
 
 */
