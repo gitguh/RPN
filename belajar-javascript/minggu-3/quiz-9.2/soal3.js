603073de2463a142d8c35944c830b6beb6a83844
Teguh <guh.sanjaya@proton.me>
2024-01-02 10:28:33 +0700

    quiz 9-2 soal 3

--- belajar-javascript/minggu-3/quiz-9.2/soal3.js
+++ belajar-javascript/minggu-3/quiz-9.2/soal3.js
@@ -1,5 +1,15 @@
 function cariMedian(arr) {
-  // you can only write your code here!
+
+  arr.sort((a,b) => a - b); // urutkan array
+
+  let nilaiTengah = Math.floor(arr.length / 2) // mencari index nilai tengah
+
+  if (arr.length % 2 === 0) { // jika array genap
+    return (arr[nilaiTengah - 1] + arr[nilaiTengah]) / 2 // kembalikan rata-rata dari dua nilai tengah >> contoh (7+8) / 2 = 7.5
+  } else { // jika array ganjil
+    return arr[nilaiTengah] // kembalikan nilai tengah
+  }
+  
 }
 
 // TEST CASES
@@ -8,3 +18,8 @@ console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
 console.log(cariMedian([3, 4, 7, 6, 10])); // 6
 console.log(cariMedian([1, 3, 3])); // 3
 console.log(cariMedian([7, 7, 8, 8])); // 7.5
+
+// console.log(cariMedian([7, 2, 10, 5, 3])) // 5
+// console.log(cariMedian([1, 3, 4, 6, 5, 2])) // 3.5
+
+// https://forum.freecodecamp.org/t/arr-sort-a-b-a-b-explanation/167677/10
