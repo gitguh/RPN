29125ef9a830aef2c7e753a842ce40336faabb91
Teguh <guh.sanjaya@proton.me>
2024-01-06 20:22:37 +0700

    ujain week 3 soal 1

---
+++ belajar-javascript/minggu-3/ujian/soal1.js
@@ -0,0 +1,74 @@
+/*
+Implementasikan function deepSum untuk mendapatkan jumlah pertambahan dari angka-angka yang terdapat di dalam array
+*/
+// https://stackoverflow.com/questions/62234969/javascript-sum-inner-arrays-in-3d-nested-arrays
+
+function deepSum (arr) {
+  let sum = 0
+
+  // Cara 1
+  // for (let i = 0; i < arr.length; i++) {
+  //   arr[i].forEach(element => {
+  //     // console.log(element)
+  //     for (let j = 0; j < element.length; j++) {
+  //       // console.log(element[j])
+  //       sum += element[j]
+  //     }
+  //   });
+  // }
+
+  // Cara 2
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      // console.log(arr[i][j])
+      for (let k = 0; k < arr[i][j].length; k++) {
+        // console.log(arr[i][j][k])
+        sum += arr[i][j][k]
+      }
+    }
+  }
+
+  return sum
+}
+
+//TEST CASE
+console.log(deepSum([
+  [
+    [4, 5, 6],
+    [9, 1, 2, 10],
+    [9, 4, 3]
+  ],
+  [
+    [4, 14, 31],
+    [9, 10, 18, 12, 20],
+    [1, 4, 90]
+  ],
+  [
+    [2, 5, 10],
+    [3, 4, 5],
+    [2, 4, 5, 10]
+  ]
+])); // 316
+
+// console.log(deepSum([
+//   [
+//     [20, 10],
+//     [15],
+//     [1, 1]
+//   ],
+//   [
+//     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+//     [2],
+//     [9, 11]
+//   ],
+//   [
+//     [3, 5, 1],
+//     [1, 5, 3],
+//     [1]
+//   ],
+//   [
+//     [2]
+//   ]
+// ])); // 156
+//
+// console.log(deepSum([])); // No number
